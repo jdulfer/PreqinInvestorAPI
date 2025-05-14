046b1b29e17af384ff665285bd9a255fc4955107
jdulfer <james.dulfer@gmail.com>
2025-05-14 17:46:31 +0100

    Load data into DB as SQLite file

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,20 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.content.negotiation)
+
     implementation(libs.exposed.core)
     implementation(libs.exposed.jdbc)
     implementation(libs.h2)
+
     implementation(libs.koin.ktor)
     implementation(libs.koin.logger.slf4j)
+
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+
+    implementation(libs.kotlin.notebook)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
